49bc3d85d1de994f7aa8170af47671fa99ddc1bb
Sejal <sejalrathore196@gmail.com>
2026-01-18 23:54:32 +0530

    day 2

--- script.js
+++ script.js
@@ -9,15 +9,25 @@
 
 //code
 
-var twosum = function(nums,target){
-    for(let i = 0;i<nums.length;i++){
-        for(let j =i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return [nums[i],nums[j]]
-            }
-        }
-    }
-}
+// var twosum = function(nums,target){
+//     for(let i = 0;i<nums.length;i++){
+//         for(let j =i+1;j<nums.length;j++){
+//             if(nums[i]+nums[j]==target){
+//                 return [nums[i],nums[j]]
+//             }
+//         }
+//     }
+// }
 
-var a = twosum([2,3,4,5,6],11);
-console.log(a)
+// var a = twosum([2,3,4,5,6],11);
+// console.log(a)
+
+
+// longest common prefix 
+
+// var longestcommonprefix =function(strs){
+// if(strs.length == 0 )return "";
+// for()
+
+// }
+
