113cab00bde67cee88f3034174b856c5f7e586b4
Sejal <sejalrathore196@gmail.com>
2026-01-18 23:48:53 +0530

    day 2

---
+++ script.js
@@ -0,0 +1,23 @@
+// arrays leetcode problems 
+
+
+// TwoSum
+
+//input = nums=[2,3,4,5,6]
+//output = target = 11
+
+
+//code
+
+var twosum = function(nums,target){
+    for(let i = 0;i<nums.length;i++){
+        for(let j =i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                return [nums[i],nums[j]]
+            }
+        }
+    }
+}
+
+var a = twosum([2,3,4,5,6],11);
+console.log(a)
