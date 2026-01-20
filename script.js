a29648099bbfaab8723ba9298ca873caab23dd24
Sejalrathore <sejalrathore1606@gmail.com>
2026-01-20 21:51:22 +0530

    day 3

--- script.js
+++ script.js
@@ -9,27 +9,32 @@
 
 //code
 
-// var twosum = function(nums,target){
-//     for(let i = 0;i<nums.length;i++){
-//         for(let j =i+1;j<nums.length;j++){
-//             if(nums[i]+nums[j]==target){
-//                 return [nums[i],nums[j]]
-//             }
-//         }
-//     }
-// }
+var twosum = function(nums,target){
+    for(let i = 0;i<nums.length;i++){
+        for(let j =i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                return [nums[i],nums[j]]
+            }
+        }
+    }
+}
 
-// var a = twosum([2,3,4,5,6],11);
-// console.log(a)
+var a = twosum([2,3,4,5,6],11);
+console.log(a)
 
 
 // longest common prefix 
 
-var longestcommonprefix =function(strs){
-if(strs.length == 0 )return "";
-for()
-
+var longestcommonprefix = function(strs){
+    if(strs.length==0)return "";
+    let prefix = strs[0];
+    for(let i =1;i<strs.length;i++){
+        while(strs[i].indexOf[prefix]!==0){
+            prefix = prefix.slice(0,-1);
+            if(prefix == "")return "";
+        }
+    }
+    return prefix;
 }
-
 
 
