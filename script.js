6b0ad949249ac0f2c6e82c3f3fc60c97a6ccf657
Sejalrathore <sejalrathore1606@gmail.com>
2026-01-20 22:46:04 +0530

    remove duplicate

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ var longestcommonprefix = function(strs){
 }
 
 
+// remove duplicates 
+
+var removeDuplicates = function(nums) {
+    if (nums.length ===0 )return 0;
+
+    let k =1;
+
+    for (let i =1;i<nums.length;i++){
+        if (nums[i] !==nums[i - 1]){
+            nums[k] = nums[i];
+            k++
+        }
+    }
+    return k;
+};
+
+
