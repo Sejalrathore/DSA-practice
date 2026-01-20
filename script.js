f73d8da0624ec0c0eed8a1fbf983036088901eeb
Sejalrathore <sejalrathore1606@gmail.com>
2026-01-20 22:54:15 +0530

    remove elements

--- script.js
+++ script.js
@@ -55,3 +55,17 @@ var removeDuplicates = function(nums) {
 };
 
 
+var removeElement = function(nums, val) {
+
+    let k = 0;
+    for (let i =0;i<nums.length;i++){
+        if (nums[i] !== val){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+return k;
+    
+};
+
